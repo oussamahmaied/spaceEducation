da597a2a39acefd9332ed267618b9076b93e47fd
oussama hmaied <ouss.hmaied.14@gmail.com>
2022-02-07 17:19:14 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const itemRoutes = require('./routes/item.routes')
+var items = require('./database-mysql');
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/../client/public"));
+
+app.use("/api/items", itemRoutes);
+
+app.listen(PORT, function () {
+  console.log("listening on port 3000!");
+});
